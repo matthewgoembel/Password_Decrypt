868683c29b90838d26f70b4b2a4464472be1e65c
Matthew Goembel <mgoembel1@gmail.com>
2023-11-23 00:29:43 -0500

    Delete src/OptimizationIdeas.java

--- src/OptimizationIdeas.java
+++
@@ -1,7 +0,0 @@
-/**
- * Goal: Optimize recursion using stacks, run in < O(26^n)?
- *
- * Approach 1: Use a modified alphabet that contains more frequently used characters at the start.
- * static final String letters = "abcdefghijklmnopqrstuvwxyz";  // In normal order
- * static final String letters = "eariotnslcudpbfghjkmqvwxyz";  // In order of frequency
-
